fc9fada7f74e25b4716f05771e8c50fd0cb43992
diriba <diribaaadunyaa@gmail.com>
2026-06-08 09:35:03 +0300

    the this problem is the most changllenging question that i fit

--- A_Anton_and_Polyhedrons.py
+++ A_Anton_and_Polyhedrons.py
@@ -1,13 +1,18 @@
-faces_map={
-  "tetrahedron":4,
-  "cube":6,
-  "octahedron":8,
-  "dodecahedron":12,
-  "icosahedron":20
+import sys
+faces_map = {
+    "Tetrahedron": 4,
+    "Cube": 6,
+    "Octahedron": 8,
+    "Dodecahedron": 12,
+    "Icosahedron": 20
 }
-n=int(input())
-total_faces=0
-for _ in range(n):
-  shape=input()
-  total_faces+=faces_map[shape]
-print(total_faces)
+input_data = sys.stdin.read().split()
+if input_data:
+    n = int(input_data[0])
+    shapes = input_data[1:]
+    
+    total_faces = 0
+    
+    for shape in shapes:
+        total_faces += faces_map[shape]
+    print(total_faces)
